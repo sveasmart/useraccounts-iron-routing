3aabbec8675df42220a085c85391ccb3d9bab842
Johan Edman <22528843+EdmanJohan@users.noreply.github.com>
2025-12-02 11:11:57 +0000

    feat: remove underscore dep

--- package.js
+++ package.js
@@ -4,38 +4,37 @@
 //   http://docs.meteor.com/#/full/packagejs
 //
 // and it is needed to define a Meteor package
-'use strict';
+"use strict";
 
 Package.describe({
-  name: 'useraccounts:iron-routing',
-  summary: 'UserAccounts package providing routes configuration capability via iron:router.',
-  version: '1.16.0',
-  git: 'https://github.com/meteor-compat/useraccounts-iron-routing.git'
+    name: "useraccounts:iron-routing",
+    summary:
+        "UserAccounts package providing routes configuration capability via iron:router.",
+    version: "1.16.1",
+    git: "https://github.com/meteor-compat/useraccounts-iron-routing.git",
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('METEOR@2.16');
+Package.onUse(function (api) {
+    api.versionsFrom("METEOR@2.16");
 
-  api.use([
-    'check',
-    'vlasky:galvanized-iron-router@2.0.1',
-    'underscore',
-    'useraccounts:core@1.16.2',
-  ], ['client', 'server']);
+    api.use(
+        [
+            "check",
+            "vlasky:galvanized-iron-router@2.0.1",
+            "useraccounts:core@1.16.2",
+        ],
+        ["client", "server"]
+    );
 
-  api.imply([
-    'useraccounts:core',
-    'vlasky:galvanized-iron-router',
-  ], ['client', 'server']);
+    api.imply(
+        ["useraccounts:core", "vlasky:galvanized-iron-router"],
+        ["client", "server"]
+    );
 
-  api.addFiles([
-    'lib/core.js',
-    'lib/server.js',
-  ], ['server']);
+    api.addFiles(["lib/core.js", "lib/server.js"], ["server"]);
 
-  api.addFiles([
-    'lib/core.js',
-    'lib/client.js',
-    'lib/templates_helpers/at_input.js',
-  ], ['client']);
+    api.addFiles(
+        ["lib/core.js", "lib/client.js", "lib/templates_helpers/at_input.js"],
+        ["client"]
+    );
 });
